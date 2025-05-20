09d9db0b1ee1724768839b7aab37bee85712e020
Oscar Eduardo Jaramillo Nuñez <38515273@claro.com.co>
2025-05-20 16:47:04 -0500

    Se implento el CategoryController

--- app/Http/Controllers/api/CategoryController.php
+++ app/Http/Controllers/api/CategoryController.php
@@ -3,47 +3,78 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $categories = Category::all();
+        return response()->json(['categories' => $categories]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:80', 'unique:categories'],
+            'description' => ['nullable', 'string']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'msg' => 'Validation error',
+                'errors' => $validator->errors(),
+                'statusCode' => 400
+            ], 400);
+        }
+
+        $category = Category::create($request->all());
+        return response()->json(['category' => $category], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $category = Category::find($id);
+        if (is_null($category)) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+        return response()->json(['category' => $category]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $category = Category::find($id);
+        if (is_null($category)) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:80', 'unique:categories,name,'.$id],
+            'description' => ['nullable', 'string']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'msg' => 'Validation error',
+                'errors' => $validator->errors(),
+                'statusCode' => 400
+            ], 400);
+        }
+
+        $category->update($request->all());
+        return response()->json(['category' => $category]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $category = Category::find($id);
+        if (is_null($category)) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+
+        $category->delete();
+        return response()->json(['categories' => Category::all(), 'success' => true]);
     }
 }
