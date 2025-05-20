3090896f96c706f1d4273a80d9187b4b85221213
Oscar Eduardo Jaramillo Nuñez <38515273@claro.com.co>
2025-05-20 09:37:16 -0500

    se acomodo una config

--- database/migrations/2025_05_20_135431_create_categories_table.php
+++ database/migrations/2025_05_20_135431_create_categories_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name',60)->unique();
+            $table->text('descripcion')->nullable();
             $table->timestamps();
         });
-    }
+    }//se agrego los campos
 
     /**
      * Reverse the migrations.
