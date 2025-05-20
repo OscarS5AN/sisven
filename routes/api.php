4f253b53df57ada2567102fc4c09a149dbdca1f1
Oscar Eduardo Jaramillo Nuñez <38515273@claro.com.co>
2025-05-20 16:50:33 -0500

    Se configuro las Rutas API

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,39 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\CustomerController;
+use App\Http\Controllers\api\CategoryController;
+use App\Http\Controllers\api\PayModeController;
+use App\Http\Controllers\api\ProductController;
 
-Route::get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
+
+// Customers Routes
+Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
+Route::post('/customers', [CustomerController::class, 'store'])->name('customers.store');
+Route::get('/customers/{customer}', [CustomerController::class, 'show'])->name('customers.show');
+Route::put('/customers/{customer}', [CustomerController::class, 'update'])->name('customers.update');
+Route::delete('/customers/{customer}', [CustomerController::class, 'destroy'])->name('customers.destroy');
+
+// Categories Routes
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+
+// Pay Modes Routes
+Route::get('/pay-modes', [PayModeController::class, 'index'])->name('pay-modes.index');
+Route::post('/pay-modes', [PayModeController::class, 'store'])->name('pay-modes.store');
+Route::get('/pay-modes/{pay_mode}', [PayModeController::class, 'show'])->name('pay-modes.show');
+Route::put('/pay-modes/{pay_mode}', [PayModeController::class, 'update'])->name('pay-modes.update');
+Route::delete('/pay-modes/{pay_mode}', [PayModeController::class, 'destroy'])->name('pay-modes.destroy');
+
+// Products Routes
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
