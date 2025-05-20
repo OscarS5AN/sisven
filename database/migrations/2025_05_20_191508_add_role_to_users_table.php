a5ee05db0dee4fc6cddc040fc9d7cb21a7266e4a
Oscar Eduardo Jaramillo Nuñez <38515273@claro.com.co>
2025-05-20 14:20:43 -0500

    en migraciones se creo los roles

--- database/migrations/2025_05_20_191508_add_role_to_users_table.php
+++ database/migrations/2025_05_20_191508_add_role_to_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->enum('role', ['admin', 'cliente', 'vendedor', 'secretaria', 'cajero'])
+              ->default('cliente')
+              ->after('password');
         });
     }
 
@@ -22,7 +24,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role');
         });
     }
 };
